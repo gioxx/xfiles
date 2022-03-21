117af41a67433ffc97e16a5306146852ba6c2fdc
Gioxx <giovanni@solone.it>
2022-03-21 14:00:55 +0100

    Piccole correzioni e porto dentro "phishingdomains_newtoday.txt"

--- sh/make_upd.sh
+++ sh/make_upd.sh
@@ -1,7 +1,8 @@
-wget -c "https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/phishing-domains/output/domains/ACTIVE/list" -O "phishingdomains.txt"
+wget -c "https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/phishing-domains-ACTIVE.txt" -O "phishingdomains.txt"
+wget -c "https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/phishing-domains-NEW-today.txt" -O "phishingdomains_newtoday.txt"
 wget -c "https://raw.githubusercontent.com/AmnestyTech/investigations/master/2021-07-18_nso/domains.txt" -O "2021-07-18_nso.txt"
 wget -c "https://raw.githubusercontent.com/tigthor/NSA-CIA-Blocklist/main/HOSTS/HOSTS" -O "NSA-CIA-Blocklist.txt"
-wget -c "https://github.com/mitchellkrogza/Phishing.Database/raw/master/whitelist.me/whitelist.me" -O "whitelist.me"
+wget -c "https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/whitelist.me/whitelist.me" -O "whitelist.me"
 
 emptycheck () {
   if [ -s $1 ]
@@ -14,6 +15,7 @@ emptycheck () {
 }
 
 emptycheck "phishingdomains.txt"
+emptycheck "phishingdomains_newtoday.txt"
 emptycheck "2021-07-18_nso.txt"
 emptycheck "NSA-CIA-Blocklist.txt"
 test -s "whitelist.me" || exit 1
@@ -26,6 +28,7 @@ rm "whitelist.txt"
 while read line; do
   echo "Cerco ed elimino $line"
   sed -e "/$line/d" -i "phishingdomains.txt"
+  sed -e "/$line/d" -i "phishingdomains_newtoday.txt"
 	sed -e "/$line/d" -i "2021-07-18_nso.txt"
 	sed -e "/$line/d" -i "NSA-CIA-Blocklist.txt"
 done < whitelist_sort.txt
@@ -33,9 +36,10 @@ done < whitelist_sort.txt
 #	Remove header and blank lines from 3rd-party lists
 #	Credits:	https://unix.stackexchange.com/questions/37790/how-do-i-delete-the-first-n-lines-of-an-ascii-file-using-shell-commands
 #           https://www.cyberciti.biz/faq/using-sed-to-delete-empty-lines/
-sed -i -e 1,3d "phishingdomains.txt"
+#sed -i -e 1,3d "phishingdomains.txt"
 sed -i -e 1,16d "NSA-CIA-Blocklist.txt"
 sed -i '/^$/d' "phishingdomains.txt"
+sed -i '/^$/d' "phishingdomains_newtoday.txt"
 
 #	Remove "0.0.0.0" (from each line) and latest two lines from NSA-CIA-Blocklist
 #	Credits:	https://stackoverflow.com/a/13380679/2220346
@@ -47,14 +51,16 @@ sed -e 's/^........//' -i "NSA-CIA-Blocklist.txt"
 #	Prepend "||" and append "^" for each line
 #	Credits:	https://stackoverflow.com/questions/2869669/in-bash-how-do-i-add-a-string-after-each-line-in-a-file
 sed -e 's/$/^/' -i "phishingdomains.txt"
+sed -e 's/$/^/' -i "phishingdomains_newtoday.txt"
 sed -e 's/$/^/' -i "2021-07-18_nso.txt"
 sed -e 's/$/^/' -i "NSA-CIA-Blocklist.txt"
 sed -e 's/^/||/' -i "phishingdomains.txt"
+sed -e 's/^/||/' -i "phishingdomains_newtoday.txt"
 sed -e 's/^/||/' -i "2021-07-18_nso.txt"
 sed -e 's/^/||/' -i "NSA-CIA-Blocklist.txt"
 
-cat "phishingdomains.txt" "2021-07-18_nso.txt" "NSA-CIA-Blocklist.txt" >> "upd_nosort.txt"
+cat "phishingdomains.txt" "phishingdomains_newtoday.txt" "2021-07-18_nso.txt" "NSA-CIA-Blocklist.txt" >> "upd_nosort.txt"
 sort -o "upd_sort.txt" "upd_nosort.txt"
 uniq "upd_sort.txt" "upd_sort_tmp.txt" && mv "upd_sort_tmp.txt" "upd_sort.txt"
 cat "upd_sort.txt" >> "upd.txt"
-rm "phishingdomains.txt" "2021-07-18_nso.txt" "NSA-CIA-Blocklist.txt" "upd_nosort.txt" "upd_sort.txt" "whitelist_sort.txt"
+rm "phishingdomains.txt" "phishingdomains_newtoday.txt" "2021-07-18_nso.txt" "NSA-CIA-Blocklist.txt" "upd_nosort.txt" "upd_sort.txt" "whitelist_sort.txt"
