d18f427ac6601499316adcb20ef32f9163a6b8e0
Gioxx <giovanni@solone.it>
2022-03-30 12:21:29 +0200

    Fix ciclo for

--- sh/make_experimental.sh
+++ sh/make_experimental.sh
@@ -28,4 +28,4 @@ cp contrib/xfiles_* ./
 ##for s in xfiles_*; do (head -n 1 "${s}" && tail -n +2 "${s}" | sort) > "sorted_${s}"; done
 ##for f in sorted_*; do (cat "${f}") >> experimental.txt; done
 
-for s in xfiles_*; do (cat "${f}") >> experimental.txt; done
+for f in xfiles_*; do (cat "${f}") >> experimental.txt; done
