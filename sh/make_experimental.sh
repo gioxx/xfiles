4d1b92885bcdb1f5f572f0c5f61cd339f364365f
Gioxx <giovanni@solone.it>
2022-03-30 12:19:27 +0200

    Rimuovo ordinamento forzato (i file sorgenti arrivano già ordinati)

--- sh/make_experimental.sh
+++ sh/make_experimental.sh
@@ -24,5 +24,8 @@ sed -i '/^$/d' "xfiles_22-bpc"
 mv "xfiles_22-bpc" contrib/
 
 cp contrib/xfiles_* ./
-for s in xfiles_*; do (head -n 1 "${s}" && tail -n +2 "${s}" | sort) > "sorted_${s}"; done
-for f in sorted_*; do (cat "${f}") >> experimental.txt; done
+
+##for s in xfiles_*; do (head -n 1 "${s}" && tail -n +2 "${s}" | sort) > "sorted_${s}"; done
+##for f in sorted_*; do (cat "${f}") >> experimental.txt; done
+
+for s in xfiles_*; do (cat "${f}") >> experimental.txt; done
