0d833048299de854fb2caf13cc686e8ab0722362
Giovanni <giovanni@solone.it>
2022-07-31 18:47:02 +0200

    Disable tigthor/NSA-CIA-Blocklist/main/HOSTS/HOSTS as requested in #11

--- sh/make_domains.sh
+++ sh/make_domains.sh
@@ -1,7 +1,7 @@
 wget -c "https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/phishing-domains-ACTIVE.txt" -O "do_phishingdomains.txt"
 wget -c "https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/phishing-domains-NEW-today.txt" -O "do_phishingdomains_newtoday.txt"
 wget -c "https://raw.githubusercontent.com/AmnestyTech/investigations/master/2021-07-18_nso/domains.txt" -O "do_2021-07-18_nso.txt"
-wget -c "https://raw.githubusercontent.com/tigthor/NSA-CIA-Blocklist/main/HOSTS/HOSTS" -O "do_NSA-CIA-Blocklist.txt"
+#wget -c "https://raw.githubusercontent.com/tigthor/NSA-CIA-Blocklist/main/HOSTS/HOSTS" -O "do_NSA-CIA-Blocklist.txt"
 wget -c "https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/whitelist.me/whitelist.me" -O "do_whitelist.me"
 
 emptycheck () {
@@ -17,7 +17,7 @@ emptycheck () {
 emptycheck "do_phishingdomains.txt"
 emptycheck "do_phishingdomains_newtoday.txt"
 emptycheck "do_2021-07-18_nso.txt"
-emptycheck "do_NSA-CIA-Blocklist.txt"
+#emptycheck "do_NSA-CIA-Blocklist.txt"
 test -s "do_whitelist.me" || exit 1
 grep -v -P '^(REG|ALL)' do_whitelist.me | sort >> do_whitelist.txt
 rm "do_whitelist.me"
@@ -30,17 +30,19 @@ while read line; do
   sed -e "/$line/d" -i "do_phishingdomains.txt"
   sed -e "/$line/d" -i "do_phishingdomains_newtoday.txt"
   sed -e "/$line/d" -i "do_2021-07-18_nso.txt"
-  sed -e "/$line/d" -i "do_NSA-CIA-Blocklist.txt"
+  #sed -e "/$line/d" -i "do_NSA-CIA-Blocklist.txt"
 done < do_whitelist_sort.txt
 
-sed -i -e 1,16d "do_NSA-CIA-Blocklist.txt"
+#sed -i -e 1,16d "do_NSA-CIA-Blocklist.txt"
 sed -i '/^$/d' "do_phishingdomains.txt"
 sed -i '/^$/d' "do_phishingdomains_newtoday.txt"
-head -n -2 "do_NSA-CIA-Blocklist.txt" > "do_NSA-CIA-Blocklist_tmp.txt" && mv "do_NSA-CIA-Blocklist_tmp.txt" "do_NSA-CIA-Blocklist.txt"
-sed -e 's/^........//' -i "do_NSA-CIA-Blocklist.txt"
+#head -n -2 "do_NSA-CIA-Blocklist.txt" > "do_NSA-CIA-Blocklist_tmp.txt" && mv "do_NSA-CIA-Blocklist_tmp.txt" "do_NSA-CIA-Blocklist.txt"
+#sed -e 's/^........//' -i "do_NSA-CIA-Blocklist.txt"
 
-cat "do_phishingdomains.txt" "do_phishingdomains_newtoday.txt" "do_2021-07-18_nso.txt" "do_NSA-CIA-Blocklist.txt" >> "do_upd_nosort.txt"
+#cat "do_phishingdomains.txt" "do_phishingdomains_newtoday.txt" "do_2021-07-18_nso.txt" "do_NSA-CIA-Blocklist.txt" >> "do_upd_nosort.txt"
+cat "do_phishingdomains.txt" "do_phishingdomains_newtoday.txt" "do_2021-07-18_nso.txt" >> "do_upd_nosort.txt"
 sort -o "do_upd_sort.txt" "do_upd_nosort.txt"
 uniq "do_upd_sort.txt" "do_upd_sort_tmp.txt" && mv "do_upd_sort_tmp.txt" "do_upd_sort.txt"
 cat "do_upd_sort.txt" >> "domains/upd_domains.txt"
-rm "do_phishingdomains.txt" "do_phishingdomains_newtoday.txt" "do_2021-07-18_nso.txt" "do_NSA-CIA-Blocklist.txt" "do_upd_nosort.txt" "do_upd_sort.txt" "do_whitelist_sort.txt"
+#rm "do_phishingdomains.txt" "do_phishingdomains_newtoday.txt" "do_2021-07-18_nso.txt" "do_NSA-CIA-Blocklist.txt" "do_upd_nosort.txt" "do_upd_sort.txt" "do_whitelist_sort.txt"
+rm "do_phishingdomains.txt" "do_phishingdomains_newtoday.txt" "do_2021-07-18_nso.txt" "do_upd_nosort.txt" "do_upd_sort.txt" "do_whitelist_sort.txt"
--- sh/make_upd.sh
+++ sh/make_upd.sh
@@ -1,7 +1,7 @@
 wget -c "https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/phishing-domains-ACTIVE.txt" -O "phishingdomains.txt"
 wget -c "https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/phishing-domains-NEW-today.txt" -O "phishingdomains_newtoday.txt"
 wget -c "https://raw.githubusercontent.com/AmnestyTech/investigations/master/2021-07-18_nso/domains.txt" -O "2021-07-18_nso.txt"
-wget -c "https://raw.githubusercontent.com/tigthor/NSA-CIA-Blocklist/main/HOSTS/HOSTS" -O "NSA-CIA-Blocklist.txt"
+#wget -c "https://raw.githubusercontent.com/tigthor/NSA-CIA-Blocklist/main/HOSTS/HOSTS" -O "NSA-CIA-Blocklist.txt"
 wget -c "https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/whitelist.me/whitelist.me" -O "whitelist.me"
 
 emptycheck () {
@@ -17,7 +17,7 @@ emptycheck () {
 emptycheck "phishingdomains.txt"
 emptycheck "phishingdomains_newtoday.txt"
 emptycheck "2021-07-18_nso.txt"
-emptycheck "NSA-CIA-Blocklist.txt"
+#emptycheck "NSA-CIA-Blocklist.txt"
 test -s "whitelist.me" || exit 1
 grep -v -P '^(REG|ALL)' whitelist.me | sort >> whitelist.txt
 rm "whitelist.me"
@@ -30,14 +30,14 @@ while read line; do
   sed -e "/$line/d" -i "phishingdomains.txt"
   sed -e "/$line/d" -i "phishingdomains_newtoday.txt"
   sed -e "/$line/d" -i "2021-07-18_nso.txt"
-  sed -e "/$line/d" -i "NSA-CIA-Blocklist.txt"
+  #sed -e "/$line/d" -i "NSA-CIA-Blocklist.txt"
 done < whitelist_sort.txt
 
 #	Remove header and blank lines from 3rd-party lists
 #	Credits:	https://unix.stackexchange.com/questions/37790/how-do-i-delete-the-first-n-lines-of-an-ascii-file-using-shell-commands
 #           https://www.cyberciti.biz/faq/using-sed-to-delete-empty-lines/
 #sed -i -e 1,3d "phishingdomains.txt"
-sed -i -e 1,16d "NSA-CIA-Blocklist.txt"
+#sed -i -e 1,16d "NSA-CIA-Blocklist.txt"
 sed -i '/^$/d' "phishingdomains.txt"
 sed -i '/^$/d' "phishingdomains_newtoday.txt"
 
@@ -45,22 +45,24 @@ sed -i '/^$/d' "phishingdomains_newtoday.txt"
 #	Credits:	https://stackoverflow.com/a/13380679/2220346
 #           https://www.baeldung.com/linux/remove-last-n-lines-of-file
 #				    https://stackoverflow.com/a/3806107/2220346
-head -n -2 "NSA-CIA-Blocklist.txt" > "NSA-CIA-Blocklist_tmp.txt" && mv "NSA-CIA-Blocklist_tmp.txt" "NSA-CIA-Blocklist.txt"
-sed -e 's/^........//' -i "NSA-CIA-Blocklist.txt"
+#head -n -2 "NSA-CIA-Blocklist.txt" > "NSA-CIA-Blocklist_tmp.txt" && mv "NSA-CIA-Blocklist_tmp.txt" "NSA-CIA-Blocklist.txt"
+#sed -e 's/^........//' -i "NSA-CIA-Blocklist.txt"
 
 #	Prepend "||" and append "^" for each line
 #	Credits:	https://stackoverflow.com/questions/2869669/in-bash-how-do-i-add-a-string-after-each-line-in-a-file
 sed -e 's/$/^/' -i "phishingdomains.txt"
 sed -e 's/$/^/' -i "phishingdomains_newtoday.txt"
 sed -e 's/$/^/' -i "2021-07-18_nso.txt"
-sed -e 's/$/^/' -i "NSA-CIA-Blocklist.txt"
+#sed -e 's/$/^/' -i "NSA-CIA-Blocklist.txt"
 sed -e 's/^/||/' -i "phishingdomains.txt"
 sed -e 's/^/||/' -i "phishingdomains_newtoday.txt"
 sed -e 's/^/||/' -i "2021-07-18_nso.txt"
-sed -e 's/^/||/' -i "NSA-CIA-Blocklist.txt"
+#sed -e 's/^/||/' -i "NSA-CIA-Blocklist.txt"
 
-cat "phishingdomains.txt" "phishingdomains_newtoday.txt" "2021-07-18_nso.txt" "NSA-CIA-Blocklist.txt" >> "upd_nosort.txt"
+#cat "phishingdomains.txt" "phishingdomains_newtoday.txt" "2021-07-18_nso.txt" "NSA-CIA-Blocklist.txt" >> "upd_nosort.txt"
+cat "phishingdomains.txt" "phishingdomains_newtoday.txt" "2021-07-18_nso.txt" >> "upd_nosort.txt"
 sort -o "upd_sort.txt" "upd_nosort.txt"
 uniq "upd_sort.txt" "upd_sort_tmp.txt" && mv "upd_sort_tmp.txt" "upd_sort.txt"
 cat "upd_sort.txt" >> "upd.txt"
-rm "phishingdomains.txt" "phishingdomains_newtoday.txt" "2021-07-18_nso.txt" "NSA-CIA-Blocklist.txt" "upd_nosort.txt" "upd_sort.txt" "whitelist_sort.txt"
+#rm "phishingdomains.txt" "phishingdomains_newtoday.txt" "2021-07-18_nso.txt" "NSA-CIA-Blocklist.txt" "upd_nosort.txt" "upd_sort.txt" "whitelist_sort.txt"
+rm "phishingdomains.txt" "phishingdomains_newtoday.txt" "2021-07-18_nso.txt" "upd_nosort.txt" "upd_sort.txt" "whitelist_sort.txt"
