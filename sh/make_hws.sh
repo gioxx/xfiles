2a68c1e15b3fd95fd9e13c2b324138dbb62ea47a
Gioxx <giovanni@solone.it>
2025-06-09 12:23:02 +0200

    Update make_hws.sh

--- sh/make_hws.sh
+++ sh/make_hws.sh
@@ -6,7 +6,10 @@ bash ./sh/parts/fetch_sources.sh ./sh/config/sources_hws.conf
 bash ./sh/parts/check_files.sh 2021-07-18_nso.txt NSA-CIA-Blocklist.txt
 bash ./sh/parts/process_urlhaus.sh
 bash ./sh/parts/process_viriback.sh
-bash ./sh/parts/apply_whitelist.sh 2021-07-18_nso.txt NSA-CIA-Blocklist.txt
-bash ./sh/parts/format_files.sh 2021-07-18_nso.txt NSA-CIA-Blocklist.txt siteblock_contrib.txt
-bash ./sh/parts/sanitize_filters.sh 2021-07-18_nso.txt NSA-CIA-Blocklist.txt siteblock_contrib.txt urlhaus_list.txt viriback_list.txt
-bash ./sh/parts/merge_and_finalize.sh siteblock.txt vcheck/check_siteblock.txt 2021-07-18_nso.txt NSA-CIA-Blocklist.txt siteblock_contrib.txt urlhaus_list.txt viriback_list.txt
+
+cp "contrib/siteblock_contrib" "hws_contrib.txt"
+
+bash ./sh/parts/apply_whitelist.sh 2021-07-18_nso.txt NSA-CIA-Blocklist.txt hws_contrib.txt
+bash ./sh/parts/format_files.sh 2021-07-18_nso.txt NSA-CIA-Blocklist.txt hws_contrib.txt
+bash ./sh/parts/sanitize_filters.sh 2021-07-18_nso.txt NSA-CIA-Blocklist.txt hws_contrib.txt urlhaus_list.txt viriback_list.txt
+bash ./sh/parts/merge_and_finalize.sh siteblock.txt vcheck/check_siteblock.txt 2021-07-18_nso.txt NSA-CIA-Blocklist.txt hws_contrib.txt urlhaus_list.txt viriback_list.txt
