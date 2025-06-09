075af2aa280618765ed31a51948241453005936d
Gioxx <giovanni@solone.it>
2025-06-09 12:37:28 +0200

    Update sanitize_filters.sh

--- sh/parts/sanitize_filters.sh
+++ sh/parts/sanitize_filters.sh
@@ -5,13 +5,15 @@ for file in "$@"; do
     [[ ! -f "$file" ]] && continue
     echo "Sanitizing filter syntax in: $file"
 
+    # Delete comments
     sed -i '/^#/d' "$file"
 
+    # Remove quotation marks and whitespace
     sed -i 's/"//g' "$file"
     sed -i 's/^[[:space:]]*//;s/[[:space:]]*$//' "$file"
     sed -i 's/[[:space:]]\+/ /g' "$file"
 
+    # Add || at the beginning and ^ at the end, if missing
     sed -i 's/$/^/' "$file"
     sed -i 's/^/||/' "$file"
 done
-
