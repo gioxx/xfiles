7e4e5df1014b41e2bb396b153aba755564d21bfc
Gioxx <giovanni@solone.it>
2025-06-09 10:35:14 +0200

    Update make_domains.sh

--- sh/make_domains.sh
+++ sh/make_domains.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 trap 'echo "❌ Error on line $LINENO. Last command: $BASH_COMMAND"' ERR
 
 bash ./sh/parts/fetch_sources.sh ./sh/config/sources_domains.conf
-bash ./sh/parts/check_files.sh do_*.txt
+ALLOW_EMPTY=true bash ./sh/parts/check_files.sh do_*.txt
 bash ./sh/parts/apply_whitelist.sh do_*.txt
 bash ./sh/parts/format_files.sh do_*.txt
 bash ./sh/parts/merge_and_finalize.sh domains/upd_domains.txt vcheck/check_domains.txt do_*.txt
