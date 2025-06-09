7322fa8622289876b70b92b2becb93309bbf2d4b
Gioxx <giovanni@solone.it>
2025-06-09 12:32:35 +0200

    Update make_upd.sh

--- sh/make_upd.sh
+++ sh/make_upd.sh
@@ -6,5 +6,5 @@ bash ./sh/parts/fetch_sources.sh ./sh/config/sources_upd.conf
 ALLOW_EMPTY=true bash ./sh/parts/check_files.sh phishingdomains.txt phishingdomains_newtoday.txt 2021-07-18_nso.txt NSA-CIA-Blocklist.txt
 bash ./sh/parts/apply_whitelist.sh phishingdomains.txt phishingdomains_newtoday.txt 2021-07-18_nso.txt NSA-CIA-Blocklist.txt
 bash ./sh/parts/format_files.sh phishingdomains.txt phishingdomains_newtoday.txt 2021-07-18_nso.txt NSA-CIA-Blocklist.txt
-bash ./sh/parts/convert_to_ublock.sh phishingdomains.txt phishingdomains_newtoday.txt 2021-07-18_nso.txt NSA-CIA-Blocklist.txt
+bash ./sh/parts/sanitize_filters.sh phishingdomains.txt phishingdomains_newtoday.txt 2021-07-18_nso.txt NSA-CIA-Blocklist.txt
 bash ./sh/parts/merge_and_finalize.sh upd.txt vcheck/check_upd.txt phishingdomains.txt phishingdomains_newtoday.txt 2021-07-18_nso.txt NSA-CIA-Blocklist.txt
