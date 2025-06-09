dd58a660d56b990aae79c4a6127660f6a58a8f7c
Gioxx <giovanni@solone.it>
2025-06-09 11:04:22 +0200

    Update make_experimental.sh

--- sh/make_experimental.sh
+++ sh/make_experimental.sh
@@ -5,9 +5,3 @@ trap 'echo "❌ Error on line $LINENO. Last command: $BASH_COMMAND"' ERR
 bash ./sh/parts/check_files.sh contrib/xfiles_*
 cp contrib/xfiles_* ./
 bash ./sh/parts/merge_preserve_order.sh experimental.txt vcheck/check_experimental.txt xfiles_*
-
-if grep -q '^stop=true' "$GITHUB_ENV"; then
-  echo "No update needed, skipping header prepend."
-else
-  bash ./sh/parts/prepend_header.sh experimental.txt "! [NoAds X Files - Filtri per blocco Paywall (sperimentale)]"
-fi
