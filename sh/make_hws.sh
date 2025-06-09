c7163128b4c1bfa716e5bbc21e1d3290b3bd28a9
Gioxx <giovanni@solone.it>
2025-06-09 12:15:10 +0200

    Update make_hws.sh

--- sh/make_hws.sh
+++ sh/make_hws.sh
@@ -8,4 +8,5 @@ bash ./sh/parts/process_urlhaus.sh
 bash ./sh/parts/process_viriback.sh
 bash ./sh/parts/apply_whitelist.sh 2021-07-18_nso.txt NSA-CIA-Blocklist.txt
 bash ./sh/parts/format_files.sh 2021-07-18_nso.txt NSA-CIA-Blocklist.txt hws_contrib.txt
+bash ./sh/parts/sanitize_filters.sh 2021-07-18_nso.txt NSA-CIA-Blocklist.txt hws_contrib.txt urlhaus_list.txt viriback_list.txt
 bash ./sh/parts/merge_and_finalize.sh siteblock.txt vcheck/check_siteblock.txt 2021-07-18_nso.txt NSA-CIA-Blocklist.txt hws_contrib.txt urlhaus_list.txt viriback_list.txt
