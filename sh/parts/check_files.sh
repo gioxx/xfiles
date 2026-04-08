e6613778506fd7eefd1c693fcec90c7f85fa2da4
Gioxx <giovanni@solone.it>
2026-04-08 14:38:47 +0200

    Update check_files.sh

--- sh/parts/check_files.sh
+++ sh/parts/check_files.sh
@@ -7,6 +7,10 @@ for file in "$@"; do
     if [[ -s "$file" ]]; then
         echo "$file is not empty"
     else
+        if [[ "$(basename -- "$file")" == "xfiles_contrib" ]]; then
+            echo "⚠️ $file is empty, but it is allowed to be optional."
+            continue
+        fi
         if [[ "$ALLOW_EMPTY" == "true" ]]; then
             echo "⚠️ $file is empty, but continuing anyway."
         else
